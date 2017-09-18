ed7a39677a57b5ae930641f2ca1050db3763f408
MontanaRoberts <montanawroberts@gmail.com>
2017-09-17 23:30:41 -0700

    adding instagram app starter code

---
+++ doggydm/db/migrate/20170910172656_add_attachment_image_to_posts.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Migration
+class AddAttachmentImageToPosts < ActiveRecord::Migration[4.2]
+  def self.up
+    change_table :posts do |t|
+      t.attachment :image
+    end
+  end
+
+  def self.down
+    remove_attachment :posts, :image
+  end
+end
